0cb4c070433af41fe42f1f40a0baab2f795caff1
stefan.roeck <stefan.roeck@7f994a3e-16b2-bcf0-713d-d90215b51390>
2010-02-18 19:26:05 +0000

    switched theme-file base implementation to usage of the RAP extension point org.eclipse.rap.ui.themeableWidgets. This is sufficient to register the custom css themes and allows an integration with other themes.

---
+++ trunk/org.eclipse.rap.rwt.commandbar/src/org/eclipse/rap/rwt/commandbar/themable/CommandBar.java
@@ -0,0 +1,20 @@
+package org.eclipse.rap.rwt.commandbar.themable;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+/**
+ * This class must not be called or referenced by clients. Its only purpose
+ * is being a place holder for the contribution at org.eclipse.rap.ui.themeableWidgets
+ * to register the custom css.
+ * @author Stefan Röck
+ * @deprecated
+ */
+@Deprecated
+public class CommandBar extends Control {
+
+	public CommandBar(Composite parent, int style) {
+		super(parent, style);
+	}
+
+}
